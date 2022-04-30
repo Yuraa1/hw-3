04fd2375760249629517221d1aaa08c67732f569
Yuraa1 <101460053+Yuraa1@users.noreply.github.com>
2022-04-30 21:22:26 +0300

    Update ATM.java
    Замінив > на >=

--- ATM.java
+++ ATM.java
@@ -3,28 +3,28 @@ class ATM {
         int a =0;
         while(sum>0){
             a++;
-            if(sum>500) {
+            if(sum>=500) {
                 sum=sum-500;
             }
-            else if(sum>200) {
+            else if(sum>=200) {
                 sum=sum-200;
             }
-            else if(sum>100) {
+            else if(sum>=100) {
                 sum=sum-100;
             }
-            else if(sum>50) {
+            else if(sum>=50) {
                 sum=sum-50;
             }
-            else if(sum>20) {
+            else if(sum>=20) {
                 sum=sum-20;
             }
-            else if(sum>10) {
+            else if(sum>=10) {
                 sum=sum-10;
             }
-            else if(sum>5) {
+            else if(sum>=5) {
                 sum=sum-5;
             }
-            else if(sum>2) {
+            else if(sum>=2) {
                 sum=sum-2;
             }
             else {
